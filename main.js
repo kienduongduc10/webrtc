ef9bfc905b8a829df257c1ae836d054348a50e4a
kienduongduc10 <93654662+kienduongduc10@users.noreply.github.com>
2021-12-18 14:43:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function openStream() {
+    const config = {audio: true, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.src0bject = stream;
+    video.play();
+}
+
+openStream()
+.then(stream => playStream('localVideo', stream));
