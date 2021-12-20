e70c2114e78a5a399e4b8db67b9f93728c8293ea
kienduongduc10 <duongduckien24082010@gmail.com>
2021-12-20 12:06:58 +0700

    commit

--- main.js
+++ main.js
@@ -9,11 +9,32 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-openStream()
-.then(stream => playStream('localVideo', stream));
+//openStream()
+//.then(stream => playStream('localVideo', stream));
 
 const peer = new Peer();
 
 peer.on('open', function(id) {
-  document.getElementById("local-peer-id").innerHTML = "Peer ID của bạn là: " + id;
+  document.getElementById("local-peer-id").innerHTML = "PeerID của bạn là: " + id;
+});
+
+//Caller
+$('#btnCall').click(() => {
+  const id = $('#remoteId').val();
+  openStream()
+  .then(stream => {
+      playStream('localStream', stream);
+      const call = peer.call(id, stream);
+      call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+  });
+});
+
+//Callee
+peer.on('call', call => {
+  openStream()
+  .then(stream => {
+      call.answer(stream);
+      playStream('localVideo', stream);
+      call.on('stream', remoteStream => playStream('remoteVideo', remoteStream));
+  });
 });
