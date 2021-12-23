57606532c5dc6f999aa775f1ed2e4702b389d448
kienduongduc10 <duongduckien24082010@gmail.com>
2021-12-23 17:24:08 +0700

    Commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const server = http.createServer();
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  console.log(socket.id);
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
